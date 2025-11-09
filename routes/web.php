e7d744cebf1b536167039e9cc88c3756c0031219
Usman-Click <47116342+Usman-Click@users.noreply.github.com>
2025-11-09 10:36:35 +0100

    feat: middlewares

--- routes/web.php
+++ routes/web.php
@@ -10,24 +10,26 @@ Route::get(
     [ListingController::class, "index"]
 );
 
+// show create form
 Route::get(
     "/listings/create",
     [ListingController::class, "create"]
-);
+)->middleware('auth');
 
 
+// store listing
 Route::post(
     "/listings",
     [ListingController::class, "store"]
-);
+)->middleware('auth');
 
 // Manage authenticated user listings
 Route::get(
     "/listings/manage",
     [ListingController::class, "manage"]
-);
+)->middleware('auth');
 
-
+// show a listings
 Route::get(
     "/listings/{id}",
     [ListingController::class, "show"]
@@ -40,13 +42,13 @@ Route::get(
 Route::get(
     "/users/create",
     [UserController::class, "create"]
-);
+)->middleware('guest');
 
 // Store user data
 Route::post(
     '/users',
     [UserController::class, 'store']
-);
+)->middleware('guest');
 
 // Log user out
 Route::post(
@@ -59,10 +61,10 @@ Route::post(
 Route::get(
     '/users/login',
     [UserController::class, 'login']
-);
+)->name('login')->middleware('guest');
 
 // auth user
 Route::post(
     '/users/auth',
     [UserController::class, 'auth']
-);
+)->middleware('guest');
